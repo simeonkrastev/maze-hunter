89b66e81f48de13c0e196877211fc25631796793
Kaloyan-Boyadzhiev <kaloyan.boyadzhiev.highschool@svetlina.net>
2023-04-03 09:51:06 +0300

    Added the most important name!

--- Maze Hunter/NameBase.cs
+++ Maze Hunter/NameBase.cs
@@ -8,7 +8,7 @@ namespace Maze_Hunter
 {
     internal class NameBase
     {
-        public string[] maleNames = {"Antiqua", "Hristo", "Maxi", "Gosho", "Valio", "Vasko", "Egor", "Nasko", "Emo",  "Kris", "Acho", "Niki", "Bobi", "Ary", "Jenkin", "Ivan" };
+        public string[] maleNames = {"Antiqua", "Hristo", "Maxi", "Gosho", "Valio", "Vasko", "Egor", "Nasko", "Emo",  "Kris", "Acho", "Niki", "Bobi", "Ary", "Jenkin", "Ivan", "Simo" };
         public string[] femaleNames = {"Bela", "Buba", "Raya", "Viki"};
     }
 }
